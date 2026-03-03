ac159726326b163f3de9e031f8f050502483d9cf
Sycholi <126089057+Sycholi@users.noreply.github.com>
2026-03-03 16:50:41 +0800

    Add color palette definitions in ColorPalette.R

---
+++ ColorPalette.R
@@ -0,0 +1,6 @@
+colorRampPalette(c("#ADC6A9", "#7DB391",'#469F80',
+                   '#16887E','#136C72','#1B5465',
+                   '#1B3955','#151D44'))(17) %>% dput()
+
+colorRampPalette(c('#376795','#528FAC','#74BCD5','#ABDCE0','#FEE6B6','#FED06E','#F7AA5C','#F08A48'))(14) %>% dput()
+colorRampPalette(c('#F0C2A2','#F091A0','#EE4866'))(7) %>% dput()
